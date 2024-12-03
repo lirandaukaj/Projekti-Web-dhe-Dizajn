b0ac9c679b0a2ae167358b79ac7f8e6f44311e3f
RuvejdaJaha <ruvejda.jahaa@gmail.com>
2024-12-03 17:27:48 +0100

    Commit14
    permiresime

---
+++ register.js
@@ -0,0 +1,6 @@
+
+function formValidation(){
+const name = document.getElementById("name").value.trim();
+const lastName = document.getElementById("lastname").value.trim();
+const email = document.getElementById("email").value.trim();
+const password = document.getElementById("password").value.trim();
