d07ce3f1f71e0b6d3bf139479d251ff8d7a4250f
RuvejdaJaha <ruvejda.jahaa@gmail.com>
2024-12-04 17:20:18 +0100

    commit16
    mainPage about us

---
+++ homePage.js
@@ -0,0 +1,12 @@
+
+//   const foto3 = document.getElementById("foto3");
+
+// function prevFunction(){
+//   const foto1 = document.getElementById("foto1");
+//   const foto1 = document.querySelector("images");
+//    foto1.style.marginLeft="0%";
+// }
+// function nextFunction(){
+//   const foto2 = document.getElementById("foto2");
+//    foto2.style.marginLeft="25%";
+// }
