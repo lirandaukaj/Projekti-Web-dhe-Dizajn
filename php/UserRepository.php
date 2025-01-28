cd8aaf222c08d64a541a25d2b086484fc10dd6c1
RuvejdaJaha <ruvejda.jahaa@gmail.com>
2025-01-28 04:12:05 +0100

    Commit 42
    crud insert get users.php

--- php/UserRepository.php
+++ php/UserRepository.php
@@ -9,10 +9,33 @@ class UserRepository{
     $this->connection = $conn->startConnection();
   }
 
-  function insertUser($user){
+  function insertUser($users){
     $conn = $this->connection;
 
-    
+    $id=$users->getId();
+    $name = $users->getName();
+    $surname = $users->getSurname();
+    $email = $users->getEmail();
+    $password = $users->getPassword();
+
+    $sql = "INSERT INTO users (id,name,surname,email,password) VALUES (?,?,?,?,?)";
+
+    $statement = $conn->prepare($sql);
+
+    $statement ->execute([$id,$name,$surname,$email,$password]);
+
+    echo "<script> alert('User has been inserted successfully!'); </script>";
+  }
+
+  function getAllUsers(){
+    $conn = $this->connection;
+
+    $sql = "SELECT - FROM users";
+
+    $statement = $conn->query($sql);
+    $users = $statement->fetchAll();
+
+    return $users;
   }
 }
 
